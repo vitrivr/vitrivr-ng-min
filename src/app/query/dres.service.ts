cc6c05bb62ed15dc71d57c686bed408317563f53
Luca Rossetto <rossetto@ifi.uzh.ch>
2023-08-24 17:38:32 +0200

    Added result set logging

--- src/app/query/dres.service.ts
+++ src/app/query/dres.service.ts
@@ -1,16 +1,23 @@
 import {Injectable} from "@angular/core";
-import {LoginRequest, SubmissionService, SuccessfulSubmissionsStatus, UserService} from "../../../openapi/dres";
+import {
+  LoginRequest,
+  LogService, QueryEvent, QueryResult as DresQueryResult, QueryResultLog,
+  SubmissionService,
+  SuccessfulSubmissionsStatus,
+  UserService
+} from "../../../openapi/dres";
 import {MediaSegmentDescriptor} from "../../../openapi/cineast";
 import {Settings} from "../settings.model";
+import {QueryResult} from "./query-result.model";
 
 @Injectable()
-export class DresService{
+export class DresService {
 
   public resultHandler: ((status: SuccessfulSubmissionsStatus) => void) | undefined;
 
   private token = ''
 
-  constructor(private submissionService: SubmissionService, private userService: UserService) {
+  constructor(private submissionService: SubmissionService, private userService: UserService, private logService: LogService) {
 
     if (Settings.dresUser.trim().length > 0) {
       userService.getApiV1User().subscribe({
@@ -39,7 +46,7 @@ export class DresService{
 
   }
 
-  public submitByTime(id: string, seconds: number){
+  public submitByTime(id: string, seconds: number) {
     const timecode = DresService.toTimecode(seconds)
     console.log("[DresService] Timecode: ", timecode);
     console.log("[DresService] Id: ", id);
@@ -53,15 +60,15 @@ export class DresService{
       timecode,
       this.token
     ).subscribe((result) => {
-      if(this.resultHandler){
+      if (this.resultHandler) {
         this.resultHandler(result);
       }
       console.log('[DresService] Submission result: ', result);
     })
   }
 
-  public submit(segment: MediaSegmentDescriptor){
-    if(!segment){
+  public submit(segment: MediaSegmentDescriptor) {
+    if (!segment) {
       console.error("Cannot submit a falsy segment!")
       return;
     }
@@ -69,7 +76,45 @@ export class DresService{
     this.submitByTime(segment?.objectId?.replace(/v_/, '') ?? 'n/a', ((segment.startabs || 0) + (segment.endabs || 0)) / 2)
   }
 
-  private static toTimecode(seconds: number):string{
+  public logResults(result: QueryResult, terms: Map<string, string>) {
+
+    if (result.objects.length == 0) {
+      return
+    }
+
+    const queryResults = result.objects.flatMap((value, index) => {
+      return {
+        item: value.id,
+        score: value.score,
+        rank: index
+
+      } as DresQueryResult;
+    }) || [];
+
+    let events = new Array<QueryEvent>();
+
+    for (const term of terms) {
+      events.push({
+        type: QueryEvent.CategoryEnum.TEXT,
+        category: term[0],
+        value: term[1],
+        timestamp: Date.now()
+      } as QueryEvent);
+    }
+
+    const resultLog = {
+      timestamp: Date.now(),
+      sortType: "",
+      resultSetAvailability: "",
+      results: queryResults,
+      events: events
+    } as QueryResultLog
+
+    this.logService.postApiV1LogResult(this.token, resultLog).subscribe();
+
+  }
+
+  private static toTimecode(seconds: number): string {
     const hours = Math.floor(seconds / 3600);
     seconds -= (hours * 3600);
     const minutes = Math.floor(seconds / 60);
@@ -77,4 +122,5 @@ export class DresService{
 
     return hours + ':' + minutes + ':' + Math.floor(seconds) + ':0';
   }
+
 }
